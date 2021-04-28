142da677f8ebab674c01fdb88bf16b37867a640b
Dimitri Halby <dimitri.halby@gmail.com>
2021-04-28 09:09:02 +0100

    ajout de view/frontend/template.php

---
+++ view/frontend/template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?= $title ?></title>
+        <link href="public/css/style.css" rel="stylesheet" /> 
+        <link href="https://en.wikipedia.org/w/load.php?lang=en&modules=ext.uls.interlanguage%7Cext.visualEditor.desktopArticleTarget.noscript%7Cext.wikimediaBadges%7Cskins.vector.styles.legacy&only=styles&skin=vector" rel="stylesheet" />
+        <link href="https://en.wikipedia.org/w/load.php?lang=en&modules=site.styles&only=styles&skin=vector" rel="stylesheet" />
+    </head>
+        
+    <body>
+        <?= $content ?>
+    </body>
+</html>
